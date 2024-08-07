d58d03cd787bb56fc3c85f721a5260ef15b19b94
Sagnik Langal <sagniklangal370@gmail.com>
2024-08-08 00:59:06 +0530

    Create string_concatenation.java

---
+++ string/string_concatenation.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("sagnik"+122+15.99);
+        
+        System.out.println("sagnik"+(122+15.99));
+        
+        System.out.println(122+15.99+"sagnik");
+        
+        // String name = "sagnik";
+        // int roll = 122;
+        // System.out.println("My name is "+name+" My roll number is "+roll);
+    }
+}

c0df1d4d1a318d41ab5bffdb8888cf894afef115
Sagnik Langal <sagniklangal370@gmail.com>
2024-08-07 17:49:14 +0530

    Create string_methods.java

---
+++ string/string_methods.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        String str = "sagnik langal";
+        // System.out.println(str.toLowerCase());
+        // System.out.println(str);
+        
+        // str = str.toLowerCase();
+        // System.out.println(str);
+        
+        // str = str.toUpperCase();
+        // System.out.println(str);
+        
+        // System.out.println(str.charAt(0)); //Character at a particular index
+        
+        //System.out.println(str.indexOf('l')); //Index of a character, always gives first ocuurence if multiple are present
+        
+        //System.out.println(str.lastIndexOf('l')); //Index of last occurence of a character
+        
+        //System.out.println(str.length()); //returns length
+        
+        /*char[] ch = str.toCharArray(); //Converting to a character array
+        for(int i=0; i<ch.length; i++){
+            System.out.print(ch[i]);
+        }*/
+        
+        // //Compare two strings
+        // System.out.println("sagnik".compareTo("sag"));
+        // System.out.println("sag".compareTo("sagnik"));
+        // System.out.println("sagnik".compareTo("sagnik"));
+        // System.out.println("sagnik".compareTo("aagnik"));
+        
+        // //Removing leading and trailing spaces
+        // System.out.println("      sagnik    ");
+        // System.out.println("      sagnik    ".trim());
+        
+        // System.out.println("sagnik".startsWith("sag")); //Prefix
+        // System.out.println("sagnik".endsWith("ik")); //Prefix
+        
+        
+    }
+}

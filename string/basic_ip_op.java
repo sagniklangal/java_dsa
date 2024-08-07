9023d83607e2b0fdb17d9b43275f32cc29d7d2f8
Sagnik Langal <sagniklangal370@gmail.com>
2024-08-07 17:08:15 +0530

    Create basic_ip_op.java

---
+++ string/basic_ip_op.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter a line");
+        String line = obj.nextLine();
+        System.out.println("Enter a word");
+        String word = obj.next();
+        System.out.println("Word is " +word);
+        System.out.println("Line is " +line);
+
+      // //Problem  
+      // Scanner obj = new Scanner(System.in);  
+      //   System.out.println("Enter a word");
+      //   String word = obj.next();  
+      //   System.out.println("Enter a line");
+      //   String line = obj.nextLine();
+      //   System.out.println("Word is " +word);
+      //   System.out.println("Line is " +line);
+    }
+}
